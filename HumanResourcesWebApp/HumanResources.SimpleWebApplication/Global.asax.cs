d78b6b3c5e1e3c506bf4f3cca6ee14ebb93d9f4e
Adrian Walsh <adyonthebeach@hotmail.co.uk>
2021-10-14 18:27:42 +0100

    Webapplication removed

--- HumanResourcesWebApp/HumanResources.SimpleWebApplication/Global.asax.cs
+++ HumanResourcesWebApp/HumanResources.SimpleWebApplication/Global.asax.cs
@@ -15,7 +15,6 @@ namespace HumanResources.SimpleWebApplication
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            UnityConfig.RegisterComponents();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
