2a01f6973e7dc9fabd85bb6015caca3ed8049bf8
Adrian Walsh <adyonthebeach@hotmail.co.uk>
2021-10-14 17:49:36 +0100

    Replaced MVC Project with new

--- HumanResourcesWebApp/HumanResources.SimpleWebApplication/Global.asax.cs
+++ HumanResourcesWebApp/HumanResources.SimpleWebApplication/Global.asax.cs
@@ -6,14 +6,13 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace HumanResources.WebApplication
+namespace HumanResources.SimpleWebApplication
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            UnityConfig.RegisterComponents();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
